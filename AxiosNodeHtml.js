bb9ef410ce014eaab9bae78ececdc925460220db
LegendSilvia <s6506021611041@email.kmutnb.ac.th>
2024-02-14 08:45:50 +0700

    Updated base url

--- AxiosNodeHtml.js
+++ AxiosNodeHtml.js
@@ -2,7 +2,7 @@ const express = require("express");
 const axios = require("axios");
 const app = express();
 var bodyParser = require("body-parser");
-const base_url = "http://localhost:3000";
+const base_url = "http://node57057-tharathep-noderest.proen.app.ruk-com.cloud";
 app.set("view engine", "ejs");
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
